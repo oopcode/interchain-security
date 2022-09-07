876277b3f456aea1a03ca4e04933d4a7f7a30dc3
Daniel <danwtisdall@gmail.com>
2022-09-07 08:37:24 +0100

    DEL things adapted from testutil/unit_test_helper

--- tests/difference/consumerStuttering/driver/setup.go
+++ tests/difference/consumerStuttering/driver/setup.go
@@ -8,15 +8,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
-	commitmenttypes "github.com/cosmos/ibc-go/v3/modules/core/23-commitment/types"
-	ibctmtypes "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 	appProvider "github.com/cosmos/interchain-security/app/provider"
-	testkeeper "github.com/cosmos/interchain-security/testutil/keeper"
 	provider "github.com/cosmos/interchain-security/x/ccv/provider"
 	providerkeeper "github.com/cosmos/interchain-security/x/ccv/provider/keeper"
-	"github.com/cosmos/interchain-security/x/ccv/provider/types"
 	providertypes "github.com/cosmos/interchain-security/x/ccv/provider/types"
 	ccv "github.com/cosmos/interchain-security/x/ccv/types"
 	"github.com/tendermint/spm/cosmoscmd"
@@ -36,49 +30,8 @@ type Runner struct {
 	lastState State
 }
 
-func fixParamSubspace(ctx sdk.Context, ss paramstypes.Subspace) paramstypes.Subspace {
-	pair := paramstypes.NewParamSetPair(types.KeyTemplateClient, &ibctmtypes.ClientState{}, func(value interface{}) error { return nil })
-	keyTable := paramstypes.NewKeyTable(pair)
-	ss = ss.WithKeyTable(keyTable)
-
-	expectedClientState :=
-		ibctmtypes.NewClientState("", ibctmtypes.DefaultTrustLevel, 0, 0,
-			time.Second*10, clienttypes.Height{}, commitmenttypes.GetSDKSpecs(), []string{"upgrade", "upgradedIBCState"}, true, true)
-
-	ss.Set(ctx, types.KeyTemplateClient, expectedClientState)
-
-	return ss
-}
-
-func GetProviderKeeperAndCtx(t testing.TB, stakingKeeper ccv.StakingKeeper) (providerkeeper.Keeper, sdk.Context) {
-
-	cdc, storeKey, paramsSubspace, ctx := testkeeper.SetupInMemKeeper(t)
-
-	paramsSubspace = fixParamSubspace(ctx, paramsSubspace)
-
-	// TODO: perhaps I can mimic the keeper creation in app.Go, only swapping out the staking keeper
-	k := providerkeeper.NewKeeper(
-		cdc,
-		storeKey,
-		paramsSubspace,
-		&testkeeper.MockScopedKeeper{},
-		&testkeeper.MockChannelKeeper{},
-		&testkeeper.MockPortKeeper{},
-		&testkeeper.MockConnectionKeeper{},
-		&testkeeper.MockClientKeeper{},
-		stakingKeeper,
-		// &SpecialStakingKeeper{},
-		// &testkeeper.MockStakingKeeper{},
-		&testkeeper.MockSlashingKeeper{},
-		&testkeeper.MockAccountKeeper{},
-		"",
-	)
-	return k, ctx
-}
-
 func NewRunner(t *testing.T, initState State) *Runner {
 	stakingKeeper := NewSpecialStakingKeeper()
-	// providerKeeper, ctx := GetProviderKeeperAndCtx(t, stakingKeeper)
 	providerKeeper, ctx := Alternative(t, stakingKeeper)
 
 	r := Runner{t, &ctx, provider.NewAppModule(&providerKeeper), &providerKeeper, stakingKeeper, initState}
